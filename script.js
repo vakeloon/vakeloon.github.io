50cccd37f5d7a87bbc2c60d0e47df26ff5a12583
vakeloon <110176977+vakeloon@users.noreply.github.com>
2024-06-09 18:24:42 +0300

    3 commit

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
 tg.expand(); //расширяем на все окно
 
 tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
-tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
-tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
+tg.MainButton.show() // показать кнопку
+tg.MainButton.enable() // сделать активной
 
-let username = "vakeloon"; // Здесь вы можете установить имя пользователя по умолчанию
+let username = tg.initDataUnsafe.user.username; // Здесь вы можете установить имя пользователя по умолчанию
 let balance = 1000; // Здесь вы можете установить начальный баланс
 
 function displayUserInfo() {
